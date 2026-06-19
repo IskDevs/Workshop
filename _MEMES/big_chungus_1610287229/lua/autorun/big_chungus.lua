30e43a197708ae57adbdc88afb0e700b24da7e7a
Gudmund1203 <107572583+Gudmund1203@users.noreply.github.com>
2026-06-20 00:09:23 +0200

    fix

---
+++ _MEMES/big_chungus_1610287229/lua/autorun/big_chungus.lua
@@ -0,0 +1,2 @@
+player_manager.AddValidModel( "Big chungus", "models/TSBB/Big_chungus.mdl")
+list.Set( "PlayerOptionsModel", "Big chungus", "models/TSBB/Big_chungus.mdl")
--- _MEMES/big_chungus_and_more_player_models_1621185363/lua/autorun/big.lua
+++
@@ -1,2 +0,0 @@
---Add PlayerModel
-player_manager.AddValidModel( "big chungus", "models/player/big/big_chungus.mdl" )
